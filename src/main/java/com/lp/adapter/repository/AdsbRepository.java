b034224473455dd7313c7cdd436d92aa3a0c0636
luo2pei4 <luo2pei4321@outlook.com>
2020-07-05 15:40:48 +0900

    主要是将数据库连接类由单例变为多例

--- src/main/java/com/lp/adapter/repository/AdsbRepository.java
+++ src/main/java/com/lp/adapter/repository/AdsbRepository.java
@@ -1,11 +1,14 @@
 package com.lp.adapter.repository;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.lp.adapter.entity.AdsbEntity;
 
-@Repository
+@Repository(value = "adsbRepository")
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public interface AdsbRepository extends JpaRepository<AdsbEntity, Long> {
 
 }
--- src/main/java/com/lp/adapter/repository/FlightInfoRepository.java
+++ src/main/java/com/lp/adapter/repository/FlightInfoRepository.java
@@ -1,14 +1,17 @@
 package com.lp.adapter.repository;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lp.adapter.entity.FlightInfoEntity;
 
-@Repository
+@Repository(value = "flightInfoRepository")
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public interface FlightInfoRepository extends JpaRepository<FlightInfoEntity, Long> {
 
 	@Query(value = "select f from FlightInfoEntity f where f.an = ?1")
-	public FlightInfoEntity searchEntityByAn(String an);
+	FlightInfoEntity searchEntityByAn(String an);
 }
